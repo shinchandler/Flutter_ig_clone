a321121013b5f374c780dca43020b49862774e80
shinchandler <pranavrishi8@gmail.com>
2023-07-20 01:30:36 +0530

    added 'ontap' in BottomNavBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void _navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: const Center(child: Text('Data')),
-        bottomNavigationBar: BottomNavigationBar(type: BottomNavigationBarType.fixed, items: const [
+        bottomNavigationBar: BottomNavigationBar(onTap: _navigateBottomNavBar, currentIndex: _selectedIndex, type: BottomNavigationBarType.fixed, items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.search_outlined), label: 'Explore'),
           BottomNavigationBarItem(icon: Icon(Icons.add_box_outlined), label: 'Post'),
